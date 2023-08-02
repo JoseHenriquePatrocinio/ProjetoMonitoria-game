8b69640dbbc78b3bc08042fe882b863a4ae83d2f
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2023-08-02 18:43:03 -0300

    feat: adicionando a ação "pulo" e "game over"

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const jorge = document.querySelector(".jorge");
+const inimigo = document.querySelector(".inimigo");
+const ponte1 = document.querySelector(".ponte1");
+const ponte2 = document.querySelector(".ponte2");
+
+const jump = () => {
+  jorge.classList.add("jump");
+  
+  setTimeout(() => {
+    jorge.classList.remove("jump");
+  }, 800);
+};
+
 setTimeout(function() {
   const inimigoElement = document.getElementById('inimigo');
   const jorgeElement = document.getElementById("jorge");
@@ -18,5 +31,38 @@ function removeImages() {
     loadImg.parentNode.removeChild(loadImg);
   }
 }
+
+const loop = setInterval(() => {
+  const inimigoPosition = inimigo.offsetLeft;
+  const ponte1Position = ponte1.offsetLeft;
+  const ponte2Position = ponte2.offsetLeft;
+  const jorgePosition = window.getComputedStyle(jorge).bottom.replace("px", "");
+
+  if (inimigoPosition < 70 && inimigoPosition > 0 && jorgePosition < 70) {
+    
+    inimigo.style.animation = "none";
+    inimigo.style.left = `${inimigoPosition}px`;
+
+    ponte1.style.animation = "none";
+    ponte1.style.left = `${ponte1Position}px`;
+
+    ponte2.style.animation = "none";
+    ponte2.style.left = `${ponte2Position}px`;
+
+    jorge.style.animation = "none";
+    jorge.style.bottom = `${jorgePosition}px`;
+
+    jorge.src = "/imgs/dano/morto.gif";
+    jorge.style.width = "150px";
+    jorge.style.marginLeft = "10px";
+    clearInterval(loop);
+  }    
+}, 10);
+
+
+
+
+document.addEventListener("click", jump);
+document.addEventListener("keydown", jump);
 
 setTimeout(removeImages, 7500);
